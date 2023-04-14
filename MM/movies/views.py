f871cb81f8e975a9380c1b814a53420a64541583
DANG417 <dndeo10814@gmail.com>
2023-04-14 09:35:22 +0900

    skeleton done

--- MM/movies/views.py
+++ MM/movies/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'movies/index.html')
+
+
+def create(request):
+    return
+
+
+def detail(request, pk):
+    return
+
+
+def update(request, pk):
+    return
+
+
+def delete(request, pk):
+    return
+
+
+def comments_create(request, pk):
+    return
+
+
+def comments_delete(request, movie_pk, comments_pk):
+    return
+
+
+def likes(request, pk):
+    return
