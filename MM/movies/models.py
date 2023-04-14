07d48d1523ce37a7797b441ed31c969ac35cc2f1
DANG417 <dndeo10814@gmail.com>
2023-04-14 09:19:47 +0900

    create model, form

--- MM/movies/models.py
+++ MM/movies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.TextField()
+    user_id = models.ForeignKey(
+        "accounts", on_delete=models.CASCADE)
