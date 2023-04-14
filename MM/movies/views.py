4780b849ac882e29fcf941a66efbbe88e7523207
DANG417 <dndeo10814@gmail.com>
2023-04-14 09:50:34 +0900

    views..ing

--- MM/movies/views.py
+++ MM/movies/views.py
@@ -11,19 +11,40 @@ def index(request):
 
 
 def create(request):
-    return
+    if request.method == "POST":
+        form = MovieForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('movies:index')
+    else:
+        form = MovieForm()
+    context = {'form': form}
+    return render(request, 'movies/create.html', context)
 
 
 def detail(request, pk):
-    return
+    movie = Movie.objects.get(pk=pk)
+    context = {'movie': movie}
+    return render(request, 'movies/detail.html', context)
 
 
 def update(request, pk):
-    return
+    movie = Movie.objects.get(pk=pk)
+    if request.method == "POST":
+        form = MovieForm(request.POST, instance=movie)
+        if form.is_valid():
+            form.save()
+            return redirect('movies:index')
+    else:
+        form = MovieForm(instance=movie)
+    context = {'form': form}
+    return render(request, 'movies/update.html', context)
 
 
 def delete(request, pk):
-    return
+    movie = Movie.objects.get(pk=pk)
+    movie.delete()
+    return redirect('movies:index')
 
 
 def comments_create(request, pk):
