ff6c8145b38905098657add9a62409c297f28290
DANG417 <dndeo10814@gmail.com>
2023-04-14 11:26:10 +0900

    user, model relation

--- MM/movies/models.py
+++ MM/movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
@@ -6,10 +7,12 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=20)
     description = models.TextField()
-    user_id = models.ForeignKey("accounts", on_delete=models.CASCADE)
+    user_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
     content = models.CharField(max_length=100)
     movie_id = models.ForeignKey("Movie", on_delete=models.CASCADE)
-    user_id = models.ForeignKey("User", on_delete=models.CASCADE)
+    user_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
