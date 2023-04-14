b1519b4c2e4be63344fb8c42d5b1cf21f57e83c2
DANG417 <dndeo10814@gmail.com>
2023-04-14 09:22:26 +0900

    modified model

--- MM/movies/models.py
+++ MM/movies/models.py
@@ -6,5 +6,10 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=20)
     description = models.TextField()
-    user_id = models.ForeignKey(
-        "accounts", on_delete=models.CASCADE)
+    user_id = models.ForeignKey("accounts", on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=100)
+    movie_id = models.ForeignKey("Movie", on_delete=models.CASCADE)
+    user_id = models.ForeignKey("User", on_delete=models.CASCADE)
